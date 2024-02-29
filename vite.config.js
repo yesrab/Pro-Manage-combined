6d60b8c6253513193ec5e4a35e8d72d2b7e1f622
yesrab <yesrab.ali111@gmail.com>
2024-02-29 17:07:37 +0530

    refactored backend

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import "dotenv/config";
+const PORT = process.env.PORT || 8080;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": `http://localhost:${8080}`,
+      "/api": `http://localhost:${PORT}`,
     },
   },
 });
